a2f6eff2c5f8f5573660c5de5558e686ea16f4e1
Maciej Smusz <maciejsmusz@gmail.com>
2018-08-29 21:06:34 -0600

    Updated so there's a splash screen

--- app/app.js
+++ app/app.js
@@ -2,56 +2,56 @@ var app = {
     socket: null,
     stage: null,
     assets: null,
-	// My game object
-    myGameObject: null,    
-    
+    // My game object
+    myGameObject: null,
+
     player2: null,
     myGameObjectColor: null,
     myGameObjectColor2: null,
     activePlayer: null,
 
-	// Values to lerp between
-	gameObjectPosition: {
-		start: {
-			x: 20,
-			y: 180
-		},
-		target: {
-			x: 150,
-			y: 200
-		}
+    // Values to lerp between
+    gameObjectPosition: {
+        start: {
+            x: 20,
+            y: 180
+        },
+        target: {
+            x: 150,
+            y: 200
+        }
     },
-	gameObjectPosition2: {
-		start: {
-			x: 20,
-			y: 300
-		},
-		target: {
-			x: 150,
-			y: 200
-		}
-	},
-	
-	// Lerp timers for both directions
-	lerpToTargetTimer: 1,
-	lerpMaxTime: 1,
+    gameObjectPosition2: {
+        start: {
+            x: 20,
+            y: 300
+        },
+        target: {
+            x: 150,
+            y: 200
+        }
+    },
 
-	// Screen Text
-	screenText: null,
+    // Lerp timers for both directions
+    lerpToTargetTimer: 1,
+    lerpMaxTime: 1,
 
-	mousePos: {x:0, y:0},
+    // Screen Text
+    screenText: null,
 
-    setupCanvas: function() {
+    mousePos: {x: 0, y: 0},
+
+    setupCanvas: function () {
         canvas = document.getElementById('game');
         canvas.width = 1200;
         canvas.height = 850;
-        canvas.addEventListener('click', function(evt) {
+        canvas.addEventListener('click', function (evt) {
             app.handleMouseDown(evt);
         });
         this.stage = new createjs.Stage(canvas);
     },
 
-    beginLoad: function() {
+    beginLoad: function () {
         manifest = [
             {
                 src: 'js/actor.js',
@@ -61,9 +61,9 @@ var app = {
             },
             // {
             //     src: 'assets/images/NewPiskel.json',
-			// 	id: 'frog',
-			// 	type: 'spritesheet',
-			// 	crossOrigin: true
+            // 	id: 'frog',
+            // 	type: 'spritesheet',
+            // 	crossOrigin: true
             // },
             {
                 src: 'assets/images/bg-temp.png',
@@ -75,13 +75,15 @@ var app = {
             }
         ];
 
-        this.assets = new createjs.LoadQueue(true); 
+        this.assets = new createjs.LoadQueue(true);
 
         createjs.Sound.alternateExtensions = ['ogg'];
 
         this.assets.installPlugin(createjs.Sound);
 
-        this.assets.on('progress', function (event) { console.log(((event.loaded / event.total) * 100) + '%'); });
+        this.assets.on('progress', function (event) {
+            console.log(((event.loaded / event.total) * 100) + '%');
+        });
 
         this.assets.on('complete', function (event) {
             app.init();
@@ -90,24 +92,24 @@ var app = {
         this.assets.loadManifest(manifest);
     },
 
-    init: function() {
+    init: function () {
         this.setupCanvas();
         this.socket = io();
 
         var stageBG = new createjs.Bitmap(app.assets.getResult('background'));
         this.stage.addChild(stageBG);
 
-        this.socket.on('newJoin', function(newPlayerID) {
+        this.socket.on('newJoin', function (newPlayerID) {
             console.log(newPlayerID + ' has just joined the game');
         });
 
-        this.socket.on('left', function(playerID) {
+        this.socket.on('left', function (playerID) {
             console.log(playerID + ' has left the game');
         });
 
-        this.socket.on('updatePosition', function(startX, startY, targetX, targetY, id) {
+        this.socket.on('updatePosition', function (startX, startY, targetX, targetY, id) {
             app.activePlayer = id;
-            if (id == "Player 1") {
+            if (id === "Player 1") {
                 app.gameObjectPosition.start.x = startX;
                 app.gameObjectPosition.start.y = startY;
                 app.gameObjectPosition.target.x = targetX;
@@ -121,11 +123,11 @@ var app = {
             app.lerpToTargetTimer = 0;
         });
 
-        this.socket.on('tooMany', function() {
+        this.socket.on('tooMany', function () {
             alert("Sorry, there are too many players so you were disconnected!");
         });
 
-        this.socket.on('new', function(x, y, a, b) {
+        this.socket.on('new', function (x, y, a, b) {
             app.gameObjectPosition.start.x = x;
             app.gameObjectPosition.start.y = y;
             app.gameObjectPosition.target.x = x;
@@ -140,11 +142,11 @@ var app = {
         // Capture our keyboard inputs and pass them down to our handler functions
         document.onkeydown = this.handleKeyDown;
         document.onkeyup = this.handleKeyUp;
-        
+
         // Set up our mouse
         this.stage.enableMouseOver();
 
-        this.stage.on("stagemousemove", function(event) {
+        this.stage.on("stagemousemove", function (event) {
             app.mousePos.x = Math.floor(event.stageX);
             app.mousePos.y = Math.floor(event.stageY);
         });
@@ -152,36 +154,35 @@ var app = {
         // Set up our update loop in the CreateJS Ticker
         createjs.Ticker.addEventListener("tick", this.update);
         createjs.Ticker.framerate = 30;
-        
-        // Setup our game
-        this.resetGame();	
 
-        this.socket.on('players2', function(players) {
-            if(players > 1) {
+        // Setup our game
+        this.resetGame();
+
+        this.socket.on('players2', function (players) {
+            if (players > 1) {
                 app.player2();
             }
         });
     },
 
-    handleMouseDown: function(evt) {
+    handleMouseDown: function (evt) {
         app.socket.emit('playerMove', this.myGameObject.x, this.myGameObject.y, this.mousePos.x, this.mousePos.y, this.player2.x, this.player2.y);
     },
 
-    resetGame: function()
-	{
-		// Create an object to move around
-		this.myGameObject = new createjs.Container();
-		this.myGameObject.x = this.gameObjectPosition.start.x;
+    resetGame: function () {
+        // Create an object to move around
+        this.myGameObject = new createjs.Container();
+        this.myGameObject.x = this.gameObjectPosition.start.x;
         this.myGameObject.y = this.gameObjectPosition.start.y;
         this.myGameObject.boundsRadius = 60;
 
-		// Create a shape to give my game object some visuals
-		var shapeRect = new createjs.Shape();
-		this.myGameObjectColor = shapeRect.graphics.beginFill('#FFF').command;
-		shapeRect.graphics.drawRect(0, 0, 100, 100);
-		this.myGameObject.addChild(shapeRect);
+        // Create a shape to give my game object some visuals
+        var shapeRect = new createjs.Shape();
+        this.myGameObjectColor = shapeRect.graphics.beginFill('#FFF').command;
+        shapeRect.graphics.drawRect(0, 0, 100, 100);
+        this.myGameObject.addChild(shapeRect);
 
-		// Add my game object to the stage
+        // Add my game object to the stage
         this.stage.addChild(this.myGameObject);
 
         // Set up the screen
@@ -190,8 +191,8 @@ var app = {
         this.scaleText.y = app.myGameObject.y;
         this.stage.addChild(this.scaleText);
     },
-    
-    player2: function() {
+
+    player2: function () {
         // Create an object to move around
         // this.player2 = new createjs.Sprite(app.assets.getResult('frog'));
         this.player2 = new createjs.Container();
@@ -213,55 +214,51 @@ var app = {
         this.scaleText2.y = app.gameObjectPosition2.start.y;
         this.stage.addChild(this.scaleText2);
     },
-	
+
     // Our game loop
-    update: function(event)
-    {
+    update: function (event) {
         app.stage.update(event);
 
         // Get our delta time (dt)
-		var dt = event.delta / 1000;
-		
-		// Check our first lerp timer, which counts up
-		if(app.lerpToTargetTimer <= app.lerpMaxTime)
-		{
-			// If the timer is below the max time, increase the timer
-			app.lerpToTargetTimer += dt;
-			
-			// If this timer is above the max, set the next timer to full
-			if(app.lerpToTargetTimer > app.lerpMaxTime)
-			{
-				app.lerpToStartTimer  = app.lerpMaxTime;
-			}
+        var dt = event.delta / 1000;
 
-			// How far are we into our lerping, OR, what percent of our lerp has occured?
-			var percentVal = app.lerpToTargetTimer / app.lerpMaxTime;
+        // Check our first lerp timer, which counts up
+        if (app.lerpToTargetTimer <= app.lerpMaxTime) {
+            // If the timer is below the max time, increase the timer
+            app.lerpToTargetTimer += dt;
 
-			app.updateGameObject(percentVal);
+            // If this timer is above the max, set the next timer to full
+            if (app.lerpToTargetTimer > app.lerpMaxTime) {
+                app.lerpToStartTimer = app.lerpMaxTime;
+            }
+
+            // How far are we into our lerping, OR, what percent of our lerp has occured?
+            var percentVal = app.lerpToTargetTimer / app.lerpMaxTime;
+
+            app.updateGameObject(percentVal);
         }
 
         if (areActorsColliding(app.myGameObject, app.player2)) {
             app.reloadGame();
         }
 
-        app.socket.on('updatePositionP1', function(newX, newY) {
+        app.socket.on('updatePositionP1', function (newX, newY) {
             app.myGameObject.x = newX;
             app.myGameObject.y = newY
         });
 
-        app.socket.on('updatePositionP2', function(newX, newY) {
+        app.socket.on('updatePositionP2', function (newX, newY) {
             app.player2.x = newX;
             app.player2.y = newY
         });
-	},
+    },
 
-	// Handles all of the lerping, called from update
-	updateGameObject: function(percentVal)
-	{
-        if(app.activePlayer == "Player 1") {
+    // Handles all of the lerping, called from update
+    updateGameObject: function (percentVal) {
+        if (app.activePlayer === "Player 1") {
             // Change the object's position
             this.myGameObject.x = this.lerp(this.gameObjectPosition.start.x, this.gameObjectPosition.target.x, percentVal);
-        
+
             // Update the text displays
             this.updateTextDisplays();
         } else {
@@ -272,21 +269,19 @@ var app = {
             // Update the text displays
             this.updateTextDisplays();
         }
-	},
+    },
 
 
-	// Our actual lerp (linear interpolation) function
-	lerp: function(valA, valB, percentVal)
-	{
-		// Clamp the percentVal
-		percentVal = percentVal > 1 ? 1 : percentVal;
-		percentVal = percentVal < 0 ? 0 : percentVal;
-		return valA * (1 - percentVal) + valB * percentVal;
-	},
+    // Our actual lerp (linear interpolation) function
+    lerp: function (valA, valB, percentVal) {
+        // Clamp the percentVal
+        percentVal = percentVal > 1 ? 1 : percentVal;
+        percentVal = percentVal < 0 ? 0 : percentVal;
+        return valA * (1 - percentVal) + valB * percentVal;
+    },
 
-	// Update all the text to show proper values
-	updateTextDisplays: function()
-	{
+    // Update all the text to show proper values
+    updateTextDisplays: function () {
         // Set up the screen
         this.scaleText.x = app.myGameObject.x;
         this.scaleText.y = app.myGameObject.y;
@@ -303,7 +298,7 @@ var app = {
         // }
     },
 
-    reloadGame: function() {
+    reloadGame: function () {
         app.socket.emit('playerMove');
         app.update();
     }
